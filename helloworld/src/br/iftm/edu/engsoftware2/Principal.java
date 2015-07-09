e05a2b9b145b8edca3b9fc19eb131fcdd379e3ac
adrianojsilva2 <adriano.vurmo@gmail.com>
2015-07-09 19:36:26 -0300

    hello world

---
+++ helloworld/src/br/iftm/edu/engsoftware2/Principal.java
@@ -0,0 +1,13 @@
+package br.iftm.edu.engsoftware2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
